359ad7989aeb94c25b70b64658f8e38752321faf
James Strachan <james.strachan@gmail.com>
2012-08-08 10:48:35 +0100

    added a helper function for kool template so we can update a template in real time based on ObservableCollections

--- kool-stream/src/main/kotlin/io/kool/collection/Extensions.kt
+++ kool-stream/src/main/kotlin/io/kool/collection/Extensions.kt
@@ -3,6 +3,7 @@ package io.kool.collection
 import java.util.Collection
 import java.io.Closeable
 import io.kool.collection.support.FunctionCollectionEventListener
+import org.w3c.dom.Element
 
 /**
  * Invokes the given block on every [[CollectionEvent<T>]]
@@ -19,5 +20,26 @@ public inline fun <T> Collection<T>.observable(): ObservableCollection<T> {
         this as (ObservableCollection<T>)
     } else {
         ObservableCollectionFacade<T>(this)
+    }
+}
+
+
+/**
+ * Allow observable collections to be bound to a DOM element
+ */
+fun <T> Element.repeat(collection: ObservableCollection<T>, block: (T)-> Unit): Unit {
+    // lets render the current collection
+    for (e in collection) {
+        // TODO we need to detect if a new element is added so that we can zap it based on the removal events
+        (block)(e)
+    }
+    collection.onCollectionEvent {
+        println("===== got collection change event ${it.kindText} on ${it.element}")
+        if (it.isRemove()) {
+            // lets remove all the child elements with the same id
+        } else {
+            // lets add/update the child elements with the element
+            (block)(it.element)
+        }
     }
 }
