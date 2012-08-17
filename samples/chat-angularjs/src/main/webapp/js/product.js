416981d897e2722fe768dd1211229e07ae9da273
James Strachan <james.strachan@gmail.com>
2012-08-17 11:08:11 +0100

    refactor to simplify js code

---
+++ samples/chat-angularjs/src/main/webapp/js/product.js
@@ -0,0 +1,34 @@
+angular.module('Koolio', ['ngResource']);
+
+function ProductController($scope, $resource, $location) {
+    var resourceURI = $location.path() + '/rest/products';
+    var Model = $resource(resourceURI);
+    $scope.results = Model.get();
+
+    $scope.elementResource = function (formData) {
+        return $resource(resourceURI + '/id/' + formData.id);
+    };
+
+    $scope.reset = function() {
+        $scope.formData = {};
+    };
+
+    $scope.startEdit = function (row) {
+        $scope.formData = row;
+    }
+
+    $scope.delete = function (formData) {
+        $scope.elementResource(formData).delete(function() {
+            // lets force a reload
+            $scope.results = Model.get();
+        });
+    };
+
+    $scope.save = function (formData) {
+        new Model(formData).$save(function() {
+            $scope.reset();
+            // lets force a reload
+            $scope.results = Model.get();
+        });
+    };
+}
