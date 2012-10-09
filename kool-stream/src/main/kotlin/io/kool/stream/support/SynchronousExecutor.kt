229f000ec7e455b6ed3adc2c8555f65a47a588c5
James Strachan <james.strachan@gmail.com>
2012-10-09 17:06:13 +0100

    fixed build with latest kotlin

--- kool-stream/src/main/kotlin/io/kool/stream/support/SynchronousExecutor.kt
+++ kool-stream/src/main/kotlin/io/kool/stream/support/SynchronousExecutor.kt
@@ -6,10 +6,9 @@ import java.util.concurrent.Executor
 * A simple synchronous implementation of [[Executor]]
 */
 class SynchronousExecutor : Executor {
-    public override fun execute(command: Runnable?) {
+    public override fun execute(command: Runnable) {
         if (command != null) {
             command.run()
         }
     }
-
 }
